f3c3ebf260d88525d017dc3f9e5e12f0cf97aa4a
davidade300 <iaderinho@icloud.com>
2026-04-07 16:55:49 -0400

    doc adicionada ao BinanceWebSocketConnector.

--- socketingestor/src/main/java/com/tradeledger/socketingestor/connector/BinanceWebSocketConnector.java
+++ socketingestor/src/main/java/com/tradeledger/socketingestor/connector/BinanceWebSocketConnector.java
@@ -23,6 +23,12 @@ public class BinanceWebSocketConnector {
     private final ObjectMapper objectMapper;
 
 
+    /**
+     * Conecta ao Websocket da Binance e publica as mensagens em uma particação do topico kafka
+     * de mensagens de trades, cada par possui uma particao propria
+     * TODO 1: Adicionar tratamento de excecao
+     * TODO 2: publicar as mensagens no kafka
+     */
     @PostConstruct
     public void ingest() {
         WebSocketClient ws = new ReactorNettyWebSocketClient();
@@ -32,8 +38,12 @@ public class BinanceWebSocketConnector {
                 .doOnNext(json -> {
                             MessageTitleRepresentation title = objectMapper.readValue(json, MessageTitleRepresentation.class);
                             log.info("{}{}", title.toKafkaKey(), json);
+
                         }
                 ).then()).subscribe();
     }
 
+    //TODO : tratar o disconect a cada 24 horas: https://developers.binance.com/docs/binance-spot-api-docs/web-socket-streams
+    public void onDisconnect() {
+    }
 }

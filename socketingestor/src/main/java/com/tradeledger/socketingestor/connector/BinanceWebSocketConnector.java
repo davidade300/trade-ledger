34a5a0d389e3566ded7e706fcec27eb5c82b72af
davidade300 <iaderinho@icloud.com>
2026-04-07 16:48:46 -0400

    websocket client funcional e logando mensagens.

--- socketingestor/src/main/java/com/tradeledger/socketingestor/connector/BinanceWebSocketConnector.java
+++ socketingestor/src/main/java/com/tradeledger/socketingestor/connector/BinanceWebSocketConnector.java
@@ -4,10 +4,12 @@ import com.tradeledger.socketingestor.representation.MessageTitleRepresentation;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
+import tools.jackson.databind.ObjectMapper;
 
 import java.net.URI;
 
@@ -18,17 +20,20 @@ import java.net.URI;
 public class BinanceWebSocketConnector {
 
     private static final String URL = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+    private final ObjectMapper objectMapper;
+
 
     @PostConstruct
-    public void listen() {
+    public void ingest() {
         WebSocketClient ws = new ReactorNettyWebSocketClient();
         ws.execute(URI.create(URL), session -> session.receive()
-                .map(WebSocketMessage::getPayloadAsText)
-                .doOnNext(System.out::println).then()).subscribe();
-    }
-
-    private MessageTitleRepresentation MessageTitle(String message) {
-        return new MessageTitleRepresentation(message);
+                .map(WebSocketMessage::getPayloadAsText
+                )
+                .doOnNext(json -> {
+                            MessageTitleRepresentation title = objectMapper.readValue(json, MessageTitleRepresentation.class);
+                            log.info("{}{}", title.toKafkaKey(), json);
+                        }
+                ).then()).subscribe();
     }
 
 }
--- socketingestor/src/main/java/com/tradeledger/socketingestor/representation/MessageTitleRepresentation.java
+++ socketingestor/src/main/java/com/tradeledger/socketingestor/representation/MessageTitleRepresentation.java
@@ -1,8 +1,13 @@
 package com.tradeledger.socketingestor.representation;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record MessageTitleRepresentation(@JsonProperty("s") String symbol) {
+
+    public String toKafkaKey() {
+        return symbol + " : ";
+    }
 }
